be95220aa455da1c5c77899cab49c914243ac7cd
vinsmith-MIB <kevincahyo.22051@mhs.unesa.ac.id>
2023-10-12 00:12:30 +0700

    ganti editor ke versi 2022

--- Assets/Script/Loading.cs
+++ Assets/Script/Loading.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loading : MonoBehaviour
+{
+
+    public Transform masukanLoadingbar;
+
+    [SerializeField]
+    private float nilaiSekarang;
+    [SerializeField]
+    private float nilaiKecepatan = 0.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nilaiSekarang < 639)
+        {
+            nilaiSekarang += nilaiKecepatan * Time.deltaTime;
+        }
+        else
+        {
+            Application.LoadLevel("Main Menu");
+        }
+        masukanLoadingbar.GetComponent<Image>().fillAmount = nilaiSekarang / 1000;
+
+    }
+}
