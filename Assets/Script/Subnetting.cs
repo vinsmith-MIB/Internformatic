f4dd07a0c4cec7eaa8c91466689cafa251869201
vinsmith-MIB <kevincahyo.22051@mhs.unesa.ac.id>
2023-12-10 17:05:06 +0700

    dfa

--- Assets/Script/Subnetting.cs
+++ Assets/Script/Subnetting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 [System.Serializable]
@@ -13,9 +14,11 @@ public class NetworkInfo
 }
 public class Subnetting : MonoBehaviour
 {
+    public Sprite newImage;
     public GameObject[] PanelJawaban;
     public string subnetmask;
     public NetworkInfo[] arrayNetworkInfo;
+    public GameObject papanScore;
     private Dictionary<NetworkInfo, bool> nilaiBooleanDictionary = new Dictionary<NetworkInfo, bool>();
 
     void Awake()
@@ -25,21 +28,22 @@ public class Subnetting : MonoBehaviour
             nilaiBooleanDictionary[nf] = false;
         }
     }
+
+    void Start(){
+        ScoreWaktu skor = papanScore.GetComponent<ScoreWaktu>();
+    }
     public void AnswerCheck()
     {
-        foreach (GameObject obj in PanelJawaban)
+        for (int pj = 0; pj < PanelJawaban.Length; pj++)
         {
-            if (obj.activeSelf)
+            if (PanelJawaban[pj].activeSelf)
             {
-                Transform child = obj.transform.GetChild(0);
                 Transform[] transformArray = new Transform[4];
-                transformArray[0] = child.transform.Find("ipaddress");
-                transformArray[1] = child.transform.Find("ipbroadcast");
-                transformArray[2] = child.transform.Find("hostpertama");
-                transformArray[3] = child.transform.Find("hostterakhir");
-                foreach (Transform t in transformArray)
-                    Debug.Log(t);
-
+                transformArray[0] = PanelJawaban[pj].transform.Find("ipaddress");
+                transformArray[1] = PanelJawaban[pj].transform.Find("ipbroadcast");
+                transformArray[2] = PanelJawaban[pj].transform.Find("hostpertama");
+                transformArray[3] = PanelJawaban[pj].transform.Find("hostterakhir");
+                Debug.Log(transformArray[0]);
                 foreach (NetworkInfo nf in arrayNetworkInfo)
                 {
                     if (!nilaiBooleanDictionary[nf])
@@ -59,9 +63,7 @@ public class Subnetting : MonoBehaviour
                                 TMP_InputField input = inputobject.GetComponent<TMP_InputField>();
                                 if (input != null)
                                 {
-                                    // Ambil nilai teks dari TMP_InputField
                                     string nilaiTeks = input.text;
-                                    Debug.Log("Nilai teks dari TMP_InputField: " + nilaiTeks);
                                 }
                                 else
                                 {
@@ -74,9 +76,21 @@ public class Subnetting : MonoBehaviour
                                 }
                             }
                         }
+
                         if (isTrue == 4)
                         {
-                            nilaiBooleanDictionar[nf] = true;
+                            nilaiBooleanDictionary[nf] = true;
+                            PanelJawaban[pj].SetActive(false);
+                            if(PanelJawaban[pj+1] != null) {
+                            PanelJawaban[pj + 1].SetActive(true);
+                            }
+                            Transform parentpanel = PanelJawaban[pj].transform.parent;
+                            Image parentimage = parentpanel.GetComponent<Image>();
+                            parentimage.sprite = newImage;
+                            skor.AddScore(100);
+                        }
+                        else{
+                            skor.AddScore(isTrue*10);
                         }
 
                     }
---
+++ Assets/Script/scorewaktu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using TMPro;
+
+public class ScoreWaktu : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI textMeshPro;
+
+    public float timer = 300f;
+    private int score = 0;
+
+    
+}
